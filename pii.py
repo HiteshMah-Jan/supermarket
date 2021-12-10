a958a7266896469712e9cd527da60a69a8ebf143
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-12-10 22:09:01 +0530

    Update pii.py

--- pii.py
+++ pii.py
@@ -13,3 +13,25 @@ int boys = 0;
 				"ifAlienAcrNo", "acrPlaceIssued", "acrDateIssued", "religion" }),
 		@DisplayGroup(gridCount = 4, title = "Contact", fields = {
 				"mobilePhone", "email", "im1", "im2", "contactNumber",
+	this.paymentTerms = paymentTerms;
+	}
+	public String getPaymentMethod() {
+		return paymentMethod;
+	}
+	public void setPaymentMethod(String paymentMethod) {
+		this.paymentMethod = paymentMethod;
+
+		def dispatch(self, request, *args, **kwargs):
+        mode = self.kwargs["mode"]
+        order = self.object = self.get_object()
+        payment_method = order.payment_method if order.payment_method_id else None
+        if mode == "payment":
+            if not order.is_paid():
+                if payment_method:
+		title = _("Payment Provider")
+    text = _("To start accepting payments right away, please add payment methods for your shop")
+    icon = "shuup_admin/img/payment.png"
+    service_model = PaymentMethod
+    base_name = "payment_method_base"
+    provider_label = _("payment method")
+    form_def_provide_key = "payment_processor_wizard_form_def"

ab2c42ab9001599c8d94852feee97f4084dfd16f
Hitesh Mahajan <hiteshvm1998@gmail.com>
2021-12-10 19:01:02 +0530

    Update pii.py

--- pii.py
+++ pii.py
@@ -5,3 +5,11 @@ int boys = 0;
             boys++;
         }
         else {
+
+            @DisplayGroup(gridCount = 6, title = "Name", fields = { "lastName",
+				"firstName", "middleInitial" }),
+		@DisplayGroup(gridCount = 4, title = "Student Info", fields = {
+				"birthDate", "age", "placeOfBirth", "nationality",
+				"ifAlienAcrNo", "acrPlaceIssued", "acrDateIssued", "religion" }),
+		@DisplayGroup(gridCount = 4, title = "Contact", fields = {
+				"mobilePhone", "email", "im1", "im2", "contactNumber",
